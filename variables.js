c034e0130e3a2863fff1c7197ad2cd996d068871
VishalDevkate <v.devkate@gmail.com>
2026-07-25 07:48:29 +0530

    more variable rules

--- variables.js
+++ variables.js
@@ -3,18 +3,19 @@ console.log(`str: ${str}`);
 console.log(`typeof str: ${typeof str}`);
 
 console.log("Demonstrating const:");
+// const radius; // This will throw an error because const variables must be initialized at the time of declaration
 const APP_URL = "https://example.com";
 console.log(`APP_URL: ${APP_URL}`);
-// APP_URL = "https://newexample.com"; // This will throw an error because APP_URL is a constant
+// APP_URL = "https://newexample.com"; // This will throw an error because APP_URL is a constant and can not be reassigned
 
 let $num = 42;
 let _num = 100;
-let num1 = 200;
+let num1 = 200, num2 = 300; //multiple variables can be declared in a single statement
 //let 2num = 300;   // This will throw an error because variable names cannot start with a number
 //let throw = "valid variable name"; //can not use reserved keywords as variable names
 console.log(`$num: ${$num}`);
 console.log(`_num: ${_num}`);
-console.log(`num1: ${num1}`);
+console.log(`num1: ${num1} num2: ${num2}`);
 
 console.log("Demonstrating scoping difference of var and let:");
 var count = 10;
@@ -23,7 +24,7 @@ console.log(`typeof count: ${typeof count}`);
 if (count > 5) {
     let message = "Count is greater than 5";
     console.log(message);
-    var count = 20; // This will not throw an error, but it will change the value of count
+    var count = 20; // This will not throw an error, but it will change the value of count outside block also. Because var doesnt support block scope.
     console.log(`the value of count(var) inside if block: ${count}`);
 }
 console.log(`the value of count(var) outside if block: ${count}`);
@@ -39,4 +40,15 @@ if (count1 > 5) {
     console.log(`the value of count(let) inside if block: ${count1}`);
 }   
 console.log(`the value of count(let) outside if block: ${count1}`);
+
+// Demostrating variable without value
+let variableWithoutValue;
+console.log(`variableWithoutValue: ${variableWithoutValue}`);
+console.log(`typeof variableWithoutValue: ${typeof variableWithoutValue}`);
+
+//cant have same variable name for other types(e.g. function name, object name)
+let greeting = "Hello";
+function greeting() {  // This will throw an error because variable name and function name are the same
+    console.log("This is a function named greeting");
+}
 
